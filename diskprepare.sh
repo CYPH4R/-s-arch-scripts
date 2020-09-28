a0f5ca269bb43722f6a050d9e7481e6368951943
CYPH3R <pitzdominik@gmail.com>
2020-09-28 22:43:40 +0200

    added swap adn changet part type

--- diskprepare.sh
+++ diskprepare.sh
@@ -3,5 +3,8 @@
 # Partitons /dev/sda in preparation for Arch installation
 # Creates Boot partition and Linux Swap partition
 
-parted -s /dev/sda mklabel msdos mkpart primary ext2 0% 90% set 1 boot on
+parted -s /dev/sda mklabel msdos mkpart primary ext4 0% 90% set 1 boot on
 parted -s /dev/sda mkpart primary linux-swap 90% 100%
+
+mkswap /dev/sda2
+swapon /dev/sda2
