5d345ccc45ee170581d37681222af48dcf2493e0
CYPH3R <pitzdominik@gmail.com>
2020-09-28 23:05:49 +0200

    see change

--- diskprepare.sh
+++ diskprepare.sh
@@ -5,7 +5,6 @@
 
 parted -s /dev/sda mklabel msdos mkpart primary ext4 0% 90% set 1 boot on
 parted -s /dev/sda mkpart primary linux-swap 90% 100%
-
 mkswap /dev/sda2
 mount /dev/sda1 /mnt
 swapon /dev/sda2
