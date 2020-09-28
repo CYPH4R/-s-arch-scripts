95e7ea9217274a225853efe5e49253f04d5b2470
CYPH3R <pitzdominik@gmail.com>
2020-09-28 23:18:42 +0200

    fixed mounting issue

--- diskprepare.sh
+++ diskprepare.sh
@@ -4,6 +4,7 @@
 # Creates Boot partition and Linux Swap partition
 
 parted -s /dev/sda mklabel msdos mkpart primary ext4 0% 90% set 1 boot on
+mkfs.ext4 /dev/sda1
 parted -s /dev/sda mkpart primary linux-swap 90% 100%
 mkswap /dev/sda2
 mount /dev/sda1 /mnt
