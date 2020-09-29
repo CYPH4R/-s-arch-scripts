c4f97efcf2a0f1e05c990fd9b8d2653b37dea83a
CYPH3R <pitzdominik@gmail.com>
2020-09-29 11:01:22 +0200

    removed swap partition

--- diskprepare.sh
+++ diskprepare.sh
@@ -3,13 +3,10 @@
 # Partitons /dev/sda in preparation for Arch installation
 # Creates Boot partition and Linux Swap partition
 
-parted -s /dev/sda mklabel msdos mkpart primary ext4 0% 90% set 1 boot on
+parted -s /dev/sda mklabel msdos mkpart primary ext4 0% 100% set 1 boot on
 mkfs.ext4 /dev/sda1
-parted -s /dev/sda mkpart primary linux-swap 90% 100%
-mkswap /dev/sda2
 mount /dev/sda1 /mnt
-swapon /dev/sda2
-pacstrap /mnt base base-devel linux linux-firmware
+pacstrap /mnt base base-devel linux linux-firmware vim
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
