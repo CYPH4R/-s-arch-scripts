afa0aa2309724b94a0db3ab4c6c5cad4be114555
CYPH3R <pitzdominik@gmail.com>
2020-09-29 13:17:38 +0200

    added user and root pwd

--- lazy.bash
+++ lazy.bash
@@ -12,3 +12,6 @@ echo "ThinkPad" > /etc/hostname
 printf "127.0.0.1\t localhost\n::1 \t \t localhost\n127.0.1.1\t ThinkPad.localdomain Thinkpad" > /etc/hosts
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
+passwd
+useradd -m -G wheel cypher
+passwd cypher
