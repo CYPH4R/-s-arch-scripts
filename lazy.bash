22e8a6511016e9ee8cb1f6ac7e8e6cbbfa7ae571
CYPH3R <pitzdominik@gmail.com>
2020-10-01 16:37:53 +0200

    changes from echo to printf for reliability

--- lazy.bash
+++ lazy.bash
@@ -6,11 +6,11 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+printf "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen
-echo "KEYMAP=de-latin1" > /etc/vconsole.conf
-echo "ThinkPad" > /etc/hostname
-printf "127.0.0.1\t localhost\n::1 \t \t localhost\n127.0.1.1\t ThinkPad.localdomain Thinkpad" > /etc/hosts
+printf "KEYMAP=de-latin1" > /etc/vconsole.conf
+printf "ThinkPad" > /etc/hostname
+printf "127.0.0.1\t localhost\n::1 \t \t localhost\n127.0.1.1\t ThinkPad.localdomain ThinkPad" > /etc/hosts
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 passwd
