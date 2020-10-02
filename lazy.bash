99b8658fe671cabca74c753ac2ba47c19a07da08
CYPH3R <pitzdominik@gmail.com>
2020-10-02 13:44:29 +0200

    small changes

--- lazy.bash
+++ lazy.bash
@@ -25,7 +25,7 @@ useradd -mG wheel cypher
 clear
 echo "Enter User Cypher Password"
 passwd cypher
-sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
+sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers
 sed -i "s/# Defaults!REBOOT !log_output/# Defaults!REBOOT !log_output\nDefaults insults/" /etc/sudoers
 #Yay Download
 git clone https://aur.archlinux.org/yay-git.git /home/cypher/yay-git
